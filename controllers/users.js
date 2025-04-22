c9a3ed855b8f04b1c3bfb32d799917aaa045488e
Ozodbek311 <serior377@gmail.com>
2025-04-22 13:43:43 +0500

    My mission

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,27 @@
 import fs from "fs"
 let users=JSON.parse(fs.readFileSync("./database/users.json","utf-8"))
 function GETALL(req,res){
-
+      return res.status(200).send(JSON.stringify(users))
 }
 
 function GETID(req,res){
-
+   try {
+      let { id } = req.query;
+      let user = users.find(el => el.id == id);
+  
+      if (user) {
+          res.status(200).json(user);
+      } else {
+          res.status(404).json({ message: "User not found" });
+      }
+   } catch(error) {
+           res.status(400).json({
+            status: 400,
+            message: "Error",
+            error: error.message
+       })
+    
+}
 }
 
 
@@ -47,9 +63,34 @@ function PUT(req,res){
 }
 
 
-function DELETE(req,res){
+function DELETE(req, res) {
+      try {
+        const { id, firstname, lastname, course, faculty } = req.query;
+        users = users.filter(el => {
+            return (
+              String(el.id) !== String(id) ||
+              String(el.firstname) !== String(firstname) ||
+              String(el.lastname) !== String(lastname) ||
+              String(el.course) !== String(course) ||
+              String(el.faculty) !== String(faculty)
+            );
+          });          
 
-}
+        fs.writeFileSync("./database/users.json", JSON.stringify(users, null, 2));
+    
+        res.status(200).json({
+          status: 200,
+          message: "Successfully deleted"
+        });
+      } catch (error) {
+        res.status(500).json({
+          status: 500,
+          message: "Error Xaltos",
+          error: error.message
+        });
+      }
+    }
+
 
 
 
